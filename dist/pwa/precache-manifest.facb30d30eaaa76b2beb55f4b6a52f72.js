9d3d4a90ba342a24190d5accb8021add7fd382d4
willerpaternostro <willergmpaternostro@gmail.com>
2020-02-16 03:26:17 -0300

    commit 16-02-2020

---
+++ dist/pwa/precache-manifest.facb30d30eaaa76b2beb55f4b6a52f72.js
@@ -0,0 +1,330 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a55bfcb91694a80f4d5",
+    "url": "/css/app.91c85675.css"
+  },
+  {
+    "revision": "5cb7edfceb233100075dc9a1e12e8da3",
+    "url": "/fonts/KFOkCnqEu92Fr1MmgVxIIzQ.5cb7edfc.woff"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/fonts/KFOlCnqEu92Fr1MmEU9fBBc-.87284894.woff"
+  },
+  {
+    "revision": "b00849e00f4c2331cddd8ffb44a6720b",
+    "url": "/fonts/KFOlCnqEu92Fr1MmSU5fBBc-.b00849e0.woff"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/fonts/KFOlCnqEu92Fr1MmWUlfBBc-.adcde98f.woff"
+  },
+  {
+    "revision": "bb1e4dc6333675d11ada2e857e7f95d7",
+    "url": "/fonts/KFOlCnqEu92Fr1MmYUtfBBc-.bb1e4dc6.woff"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/fonts/KFOmCnqEu92Fr1Mu4mxM.60fa3c06.woff"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/fonts/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/fonts/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/fonts/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/fonts/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/fonts/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/fonts/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "29b882f018fa6fe75fd338aaae6235b8",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNa.29b882f0.woff"
+  },
+  {
+    "revision": "0509ab09c1b0d2200a4135803c91d6ce",
+    "url": "/fonts/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.0509ab09.woff2"
+  },
+  {
+    "revision": "53677ffeb133eeccc5b29372d03468b3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d85769fb94539f11b696",
+    "url": "/js/2.422ca73a.js"
+  },
+  {
+    "revision": "cec7257c1d12834ca54e",
+    "url": "/js/3.7deb671e.js"
+  },
+  {
+    "revision": "355fee96c7ffd482a7f2",
+    "url": "/js/4.5e6ebb29.js"
+  },
+  {
+    "revision": "f79124eb23f0514753e1",
+    "url": "/js/5.14d02691.js"
+  },
+  {
+    "revision": "b88c8f865e2e0e458185",
+    "url": "/js/6.e9124595.js"
+  },
+  {
+    "revision": "b940f05226702c7980c6",
+    "url": "/js/7.8b9f8db5.js"
+  },
+  {
+    "revision": "2627dc473b2f86ec8ac6",
+    "url": "/js/8.2621d56c.js"
+  },
+  {
+    "revision": "865129cfb3f24709a39e",
+    "url": "/js/9.09d82cae.js"
+  },
+  {
+    "revision": "4a55bfcb91694a80f4d5",
+    "url": "/js/app.5f86413f.js"
+  },
+  {
+    "revision": "d18fd4239498c8ada21c",
+    "url": "/js/vendor.fb5e3872.js"
+  },
+  {
+    "revision": "3dd76b9cb8f72f6deddcca83d84b233e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "43aaa5717169a9cab1bde8de2d0c3701",
+    "url": "/statics/app-logo-128x128.png"
+  },
+  {
+    "revision": "7b288b92eeb654adf335c711e949f1d4",
+    "url": "/statics/icons/adilso.jpg"
+  },
+  {
+    "revision": "bde75c95c26cbe407d8c3a51149c9047",
+    "url": "/statics/icons/apple-icon-120x120.png"
+  },
+  {
+    "revision": "1e6959a9778d40656217fe5eb7e0b7aa",
+    "url": "/statics/icons/apple-icon-152x152.png"
+  },
+  {
+    "revision": "e776afac4479ee2ad8d6a0397661bf09",
+    "url": "/statics/icons/apple-icon-167x167.png"
+  },
+  {
+    "revision": "37ab9045dbdc0edf12ea59b3772e19f9",
+    "url": "/statics/icons/apple-icon-180x180.png"
+  },
+  {
+    "revision": "de9aa17c0f5753fac1f1e0672fca2c02",
+    "url": "/statics/icons/bixos.jpg"
+  },
+  {
+    "revision": "2daf0dc2e18ce0913fc6dca41e883c1e",
+    "url": "/statics/icons/bleiner.jpg"
+  },
+  {
+    "revision": "ef1eddf9bf69c95b887deac55951bc61",
+    "url": "/statics/icons/caads-dark.png"
+  },
+  {
+    "revision": "a6878ae3a5f052d6a796a7a12fa69cca",
+    "url": "/statics/icons/caads-transparente.png"
+  },
+  {
+    "revision": "462a9bdc3418c957f774f32d09e3ab01",
+    "url": "/statics/icons/caads-white.png"
+  },
+  {
+    "revision": "dd5e7016663945e21724b14c6c2ba86c",
+    "url": "/statics/icons/camila.jpg"
+  },
+  {
+    "revision": "2e73c1db48b0fcb2eb1870796059ce7d",
+    "url": "/statics/icons/camiseta-costas.jpg"
+  },
+  {
+    "revision": "feb66b57e8b61d66bf2b04c02e217d80",
+    "url": "/statics/icons/camiseta-frente.jpg"
+  },
+  {
+    "revision": "f063dca41d1bf0664b99b64c70f046be",
+    "url": "/statics/icons/caneca.jpg"
+  },
+  {
+    "revision": "266e53e77e0eb11ca217351694a4ffaf",
+    "url": "/statics/icons/chaveiros.jpg"
+  },
+  {
+    "revision": "fea92f3bce82274f2f082b3894bd043c",
+    "url": "/statics/icons/construcao.jpg"
+  },
+  {
+    "revision": "751db1347d2e04fad5b95e12e71f4a16",
+    "url": "/statics/icons/curso.jpg"
+  },
+  {
+    "revision": "cc72fd55b71a04941842927c47b5fa0e",
+    "url": "/statics/icons/doll-costas.jpg"
+  },
+  {
+    "revision": "316862dc6c3cb7ae6fceb6521cd24eca",
+    "url": "/statics/icons/doll-frente.jpg"
+  },
+  {
+    "revision": "a33f77a231f12ce8eead9d65aa30ddea",
+    "url": "/statics/icons/eduardo.jpg"
+  },
+  {
+    "revision": "bcff49a0cc00d59947c007e378aac39e",
+    "url": "/statics/icons/events.jpg"
+  },
+  {
+    "revision": "1821eacb27606d96c9a5168e1839e438",
+    "url": "/statics/icons/fabricio.jpg"
+  },
+  {
+    "revision": "d3a6bb0a3f07cb66bc8db135a47096e5",
+    "url": "/statics/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "8d2ada8801fd02414b7fac74cf675737",
+    "url": "/statics/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "50c375e799f09c82eb9ae714b6613fad",
+    "url": "/statics/icons/favicon.ico.png"
+  },
+  {
+    "revision": "8d2ada8801fd02414b7fac74cf675737",
+    "url": "/statics/icons/favicon.png"
+  },
+  {
+    "revision": "097e71cf621911ebf40eabaa05e4c800",
+    "url": "/statics/icons/gabriela.jpg"
+  },
+  {
+    "revision": "0760a113f9052073a475f693822f9c5c",
+    "url": "/statics/icons/giovani.jpg"
+  },
+  {
+    "revision": "5300bc18a77ebf892df2982122347781",
+    "url": "/statics/icons/icon-128x128.png"
+  },
+  {
+    "revision": "d6199586e62ccaa0277922bcab59091e",
+    "url": "/statics/icons/icon-192x192.png"
+  },
+  {
+    "revision": "5513f64d403d38dde8741c6431edf504",
+    "url": "/statics/icons/icon-256x256.png"
+  },
+  {
+    "revision": "b8762781da25e69b991a438d74ef4756",
+    "url": "/statics/icons/icon-384x384.png"
+  },
+  {
+    "revision": "43aaa5717169a9cab1bde8de2d0c3701",
+    "url": "/statics/icons/icon-512x512.png"
+  },
+  {
+    "revision": "5c08cac9ad5782d2e8e5a5da93fc9e80",
+    "url": "/statics/icons/ifsp-logo.png"
+  },
+  {
+    "revision": "c05a75101147949bac5e26d5c39ea2ee",
+    "url": "/statics/icons/ifsp-sanca.jpg"
+  },
+  {
+    "revision": "7dcedc2aa1605da01b0e47a047c4f5c6",
+    "url": "/statics/icons/kit-completo.jpg"
+  },
+  {
+    "revision": "1a99c38503d3a0e450a3cad8a0dbb62a",
+    "url": "/statics/icons/lorrayne.jpg"
+  },
+  {
+    "revision": "bd9b74c408e07d11cf643f0bb85a1691",
+    "url": "/statics/icons/ms-icon-144x144.png"
+  },
+  {
+    "revision": "2e6f5648553b62c677c1639662f9636d",
+    "url": "/statics/icons/parceiros.jpg"
+  },
+  {
+    "revision": "5938e58829a18e7a997aa501f8e8136a",
+    "url": "/statics/icons/patrocinio.jpg"
+  },
+  {
+    "revision": "36b557a65c5e665553b8be57dbbf79fc",
+    "url": "/statics/icons/rebeca.jpg"
+  },
+  {
+    "revision": "4c854e3f54e50b3af0487c8f6c7c3cef",
+    "url": "/statics/icons/regata-costas.jpg"
+  },
+  {
+    "revision": "9931573c1e110d3586f2529467e9b90f",
+    "url": "/statics/icons/regata-frente.jpg"
+  },
+  {
+    "revision": "84666e32b20f746bec88b7a24941b3ac",
+    "url": "/statics/icons/roger.jpg"
+  },
+  {
+    "revision": "188289588a649cf953268350c273c81b",
+    "url": "/statics/icons/sacochila.jpg"
+  },
+  {
+    "revision": "d9ab6af08e2472f30bc5e4f9d15fd547",
+    "url": "/statics/icons/samba-costas.jpg"
+  },
+  {
+    "revision": "9dddd6f7d3cba8fb53780640894bd53e",
+    "url": "/statics/icons/samba-frente.jpg"
+  },
+  {
+    "revision": "71aece754ddf322df4fb18331442122c",
+    "url": "/statics/icons/sanca.jpg"
+  },
+  {
+    "revision": "d6bad6a973f079c0a20d6b431e71ae70",
+    "url": "/statics/icons/suap.png"
+  },
+  {
+    "revision": "2bf4cd624aea093827dcbeaf3d5775e4",
+    "url": "/statics/icons/suzy.jpg"
+  },
+  {
+    "revision": "f14735fabef677a52666c5988f579162",
+    "url": "/statics/icons/tirante.jpg"
+  },
+  {
+    "revision": "9c2b797265fb2ba39aec401d8d96f072",
+    "url": "/statics/icons/vagas.jpg"
+  },
+  {
+    "revision": "6b8b14294a23801398c73cb2a82912e1",
+    "url": "/statics/icons/willer.jpg"
+  },
+  {
+    "revision": "28ddbcf41cda9eebac66c007c10d84c0",
+    "url": "/statics/icons/yuri.jpg"
+  }
+]);
